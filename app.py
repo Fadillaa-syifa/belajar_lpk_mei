3840caea03044c213e510645f2462447a41b9e46
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 10:52:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,3 +115,12 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+import pandas as pd
+import streamlit as st
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
