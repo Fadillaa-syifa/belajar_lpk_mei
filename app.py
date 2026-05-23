918e5e62284e0dc894d3710988c2f4a51b8a901f
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 10:40:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,3 +72,14 @@ with col2:
 with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = rng(0).standard_normal((10, 1))
+col1, col2 = st.columns([3, 1])
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(df)
+
+col2.subheader("A narrow column with the data")
+col2.write(df)
