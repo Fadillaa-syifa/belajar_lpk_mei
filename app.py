9ce06c3c3ea921be70be4a627dd2ca846abeffbb
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 10:31:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,19 @@ if middle.button("Emoji button", icon="😃", width="stretch"):
     middle.markdown("You clicked the emoji button.")
 if right.button("Material button", icon=":material/mood:", width="stretch"):
     right.markdown("You clicked the Material button.")
+import streamlit as st
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
+
+if genre == ":rainbow[Comedy]":
+    st.write("You selected comedy.")
+else:
+    st.write("You didn't select comedy.")
