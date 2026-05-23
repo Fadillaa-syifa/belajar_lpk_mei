ac72aa6a1590699d9dc87ea1c1a79c498625f841
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 10:52:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -122,5 +122,5 @@ uploaded_files = st.file_uploader(
     "Upload data", accept_multiple_files=True, type="csv"
 )
 for uploaded_file in uploaded_files:
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_pdf(uploaded_file)
     st.write(df)
