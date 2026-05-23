00880dddc146ddbb15c082926ed036a477279c11
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 10:30:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,12 @@ else:
 
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
+import streamlit as st
+
+left, middle, right = st.columns(3)
+if left.button("Plain button", width="stretch"):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", width="stretch"):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", width="stretch"):
+    right.markdown("You clicked the Material button.")
