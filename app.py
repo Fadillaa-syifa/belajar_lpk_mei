fb8b85d67585cd682d2c576ba639da8df878fe91
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 10:48:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,3 +83,14 @@ col1.line_chart(df)
 
 col2.subheader("A narrow column with the data")
 col2.write(df)
+import streamlit as st
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
